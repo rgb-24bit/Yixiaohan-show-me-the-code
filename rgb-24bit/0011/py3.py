dbc705bcb33943632bb8bf7036ad88003fee002d
rgb-24bit <rgb-24bit@foxmail.com>
2018-02-23 14:53:50 +0800

    0011: 完善代码， Python3 确实方便

--- rgb-24bit/0011/py3.py
+++ rgb-24bit/0011/py3.py
@@ -3,12 +3,12 @@
 
 def get_filtered_words(file_name='filtered_words.txt'):
     with open(file_name, 'r', encoding='utf-8') as fp:
-        filtered_words = [word.encode('utf-8').strip() for word in fp]
+        filtered_words = [word.strip() for word in fp]
     return filtered_words
 
 
 def get_user_input():
-    return input('Input: ').encode('utf-8')
+    return input('Input: ')
 
 
 def prog_output(user_input, filtered_words):
