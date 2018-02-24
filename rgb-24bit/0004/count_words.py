15aa0d52143625e63371de4c3a284c04d5b02541
rgb-24bit <rgb-24bit@foxmail.com>
2018-02-24 17:07:25 +0800

    0004: 完成 第 0004 题

---
+++ rgb-24bit/0004/count_words.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import re
+
+
+def get_text(file_name='words.txt'):
+    with open(file_name, 'r') as fp:
+        return fp.read()
+
+
+def get_word_count(text):
+    pattern = r'\b(\w+?)\b'
+    return len(re.findall(pattern, text))
+
+
+if __name__ == '__main__':
+    text = get_text()
+    print(get_word_count(text))
+
