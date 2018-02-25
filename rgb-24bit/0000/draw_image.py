0566f558aeaf359e8ecada47bfacf228d399fa23
rgb-24bit <rgb-24bit@foxmail.com>
2018-02-25 16:13:14 +0800

    0000: 完成 第 0000 题

---
+++ rgb-24bit/0000/draw_image.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+def get_img(img_name):
+    return Image.open(img_name)
+
+
+def get_font(img, font_name):
+    w, h = img.size
+    return ImageFont.truetype(font_name, h / 5)
+
+
+def draw_num(img, font, num):
+    w, h = img.size
+    draw = ImageDraw.Draw(img)
+    draw.text((w / 5 * 4, h / 5), str(num), fill=(102, 204, 255), font=font)
+    return img
+
+
+if __name__ == '__main__':
+    img = get_img('avatar.jpg')
+    font = get_font(img, 'arial.ttf')
+    draw_num(img, font, 6).save('new.jpg')
