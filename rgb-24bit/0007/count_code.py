fb4bd7757f4b699a152d400bde0d2aa6fdadae6a
rgb-24bit <rgb-24bit@foxmail.com>
2018-02-26 14:52:09 +0800

    0007: 完成 第 0007 题
    问题: 行内注释处理

---
+++ rgb-24bit/0007/count_code.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import os
+
+
+def get_file_list(dir_name):
+    file_type = '.py'
+    for file_name in os.listdir(dir_name):
+        if file_type == os.path.splitext(file_name)[-1]:
+            yield os.path.join(dir_name, file_name)
+
+
+def get_code_amount(file_name):
+    key = ['code', 'blank', 'comment']
+    code_amount = dict.fromkeys(key, 0)
+
+    with open(file_name, 'r') as fp:
+        for line in fp:
+            if line.strip():
+                if line[0] == '#':
+                    code_amount['comment'] += 1
+                else:
+                    code_amount['code'] += 1
+            else:
+                code_amount['blank'] += 1
+
+    return code_amount
+
+
+if __name__ == '__main__':
+    key = ['code', 'blank', 'comment']
+    all_code_amount = dict.fromkeys(key, 0)
+
+    for file_name in get_file_list('../0012'):
+        code_amount = get_code_amount(file_name)
+        for key, val in code_amount.items():
+            all_code_amount[key] += val
+
+    for key, val in all_code_amount.items():
+        print(key, val)
