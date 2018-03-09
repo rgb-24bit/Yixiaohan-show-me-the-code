60e68137849c136496e158417bcece2c196c8963
rgb-24bit <rgb-24bit@foxmail.com>
2018-03-09 19:16:30 +0800

    0017: 修改 第 0017 题， 还是有问题

--- rgb-24bit/0017/to_xml.py
+++ rgb-24bit/0017/to_xml.py
@@ -4,38 +4,40 @@ import json
 import xlrd
 
 from collections import OrderedDict
-from xml.dom import minidom
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+from xml.dom.minidom import parseString
 
 
-def get_data(file_name='student.xls'):
+def get_data_dict(file_name='student.xls'):
     with xlrd.open_workbook(file_name) as fp:
-        sheet = fp.sheet_by_index(0)
-        data = OrderedDict()
+        sheet, data = fp.sheet_by_index(0), OrderedDict()
 
         for i in range(sheet.nrows):
-            data[str(i+1)] = sheet.row_values(i)[1:]
+            data[str(i + 1)] = sheet.row_values(i)[1:]
 
         return data
 
 
-def gen_xml(file_name, root, child, comment, data):
-    impl = minidom.getDOMImplementation()
-    new_doc = impl.createDocument(None, root, None)
+def get_xml(root, child, comment_str, data_dict):
+    root = Element(root)
+    root.append(Comment(text=comment_str.decode('utf-8')))
 
-    node_students = new_doc.createElement(child)
-    node_students.appendChild(new_doc.createComment(comment))
-    node_students.appendChild(new_doc.createTextNode(data))
+    text = json.dumps(data_dict, ensure_ascii=False, indent=4)
+    SubElement(root, child).text = '\n' + text + '\n'
 
-    node_root = new_doc.documentElement
-    node_root.appendChild(node_students)
+    return tostring(root, encoding='utf-8')
 
+
+def gen_xml(file_name, xml):
     with open(file_name, 'w') as fp:
-        new_doc.writexml(fp, newl='\n', encoding='utf-8')
-
+        dom = parseString(xml)
+        text = dom.toprettyxml(encoding='utf-8')
+        fp.write(text.replace('&quot;', '"'))
 
 
 if __name__ == '__main__':
-    data = get_data()
+    data = get_data_dict()
     file_name = 'students.xml'
     comment = '\n\t学生信息表\n\t"id" : [名字, 数学, 语文, 英文]\n'
-    gen_xml(file_name, 'root', 'students', comment, json.dumps(data, indent=4))
+    xml = get_xml('root', 'students', comment, data)
+    gen_xml(file_name, xml)
