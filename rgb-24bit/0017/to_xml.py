c59889ffbf8083b24a9581dde8629f81ae15ac3a
rgb-24bit <rgb-24bit@foxmail.com>
2018-03-10 17:01:00 +0800

    0017: 完善 第 0017 题， 就这样了， 。。。

--- rgb-24bit/0017/to_xml.py
+++ rgb-24bit/0017/to_xml.py
@@ -3,41 +3,34 @@
 import json
 import xlrd
 
+from lxml import etree
 from collections import OrderedDict
-from xml.etree.ElementTree import Element, SubElement, Comment, tostring
-from xml.dom.minidom import parseString
 
 
 def get_data_dict(file_name='student.xls'):
     with xlrd.open_workbook(file_name) as fp:
-        sheet, data = fp.sheet_by_index(0), OrderedDict()
+        sheet, data_dict = fp.sheet_by_index(0), OrderedDict()
 
         for i in range(sheet.nrows):
-            data[str(i + 1)] = sheet.row_values(i)[1:]
+            data_dict[str(i + 1)] = sheet.row_values(i)[1:]
 
-        return data
-
-
-def get_xml(root, child, comment_str, data_dict):
-    root = Element(root)
-    root.append(Comment(text=comment_str.decode('utf-8')))
-
-    text = json.dumps(data_dict, ensure_ascii=False, indent=4)
-    SubElement(root, child).text = '\n' + text + '\n'
-
-    return tostring(root, encoding='utf-8')
+        return data_dict
 
 
-def gen_xml(file_name, xml):
-    with open(file_name, 'w') as fp:
-        dom = parseString(xml)
-        text = dom.toprettyxml(encoding='utf-8')
-        fp.write(text.replace('&quot;', '"'))
+def get_xml(file_name, root, child, comment_str, data_dict):
+    root = etree.Element(root)
+    students = etree.SubElement(root, child)
+
+    comment = etree.Comment(comment_str)
+    comment.tail = json.dumps(data_dict, ensure_ascii=False)
+
+    students.append(comment)
+
+    tree = etree.ElementTree(root)
+    tree.write(file_name, pretty_print=True, encoding='utf-8', xml_declaration=True)
 
 
 if __name__ == '__main__':
-    data = get_data_dict()
-    file_name = 'students.xml'
-    comment = '\n\t学生信息表\n\t"id" : [名字, 数学, 语文, 英文]\n'
-    xml = get_xml('root', 'students', comment, data)
-    gen_xml(file_name, xml)
+    data_dict = get_data_dict()
+    comment = u'学生信息表"id" : [名字, 数学, 语文, 英文]'
+    get_xml('students.xml', 'root', 'students', comment, data_dict)
