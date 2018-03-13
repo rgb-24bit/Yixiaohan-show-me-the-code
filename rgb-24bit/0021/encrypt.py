08136f3ae80a225d73d54a91ce33846ece764208
rgb-24bit <rgb-24bit@foxmail.com>
2018-03-13 12:31:33 +0800

    0021: 修改 第 0021 题

--- rgb-24bit/0021/encrypt.py
+++ rgb-24bit/0021/encrypt.py
@@ -2,23 +2,37 @@
 
 from __future__ import print_function
 
+import io
+import os
 import sys
 
 from hashlib import sha256
+from hmac import HMAC
+
+def encrypt_password(password, salt=None):
+    """Hash password on the fly."""
+    if salt is None:
+        salt = os.urandom(8)
+
+    for i in range(10):
+        try:
+            password = HMAC(password, salt, sha256).hexdigest().encode('utf-8')  # python3 的返回值是 unicode
+        except TypeError:
+            password = HMAC(password, salt, sha256).hexdigest()
+
+    return salt + password  # 保存每个账号的 salt
 
 
-def encrypt_pwd(pwd):
-    return sha256(pwd).hexdigest()
 
-
-def get_pwd():
+def get_password():
     try:
-        pwd = raw_input('Input password: ').decode(sys.stdin.encoding)
-        pwd = pwd.encode('utf-8')
+        password = raw_input('Input password: ').decode(sys.stdin.encoding)
+        password = password.encode('utf-8')
     except NameError:
-        pwd = input('Input password: ').encode('utf-8')
+        password = input('Input password: ').encode('utf-8')
 
-    return pwd
+    return password
+
 
 if __name__ == '__main__':
-    print(encrypt_pwd(get_pwd()))
+    print(encrypt_password(get_password()))
